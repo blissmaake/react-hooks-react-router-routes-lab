b37e5b4d36c967ada21baa0df7cb82ef4ab8d503
Bliss <blissmaake140@gmail.com>
2023-04-28 16:06:30 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import React from "react"
+import { Route, Switch } from "react-router-dom"
+import NavBar from "./NavBar"
+import Home from "./Home"
+import Actors from "./Actors"
+import Directors from "./Directors"
+import Movies from "./Movies"
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/actors" element={<Actors />} />
+        <Route path="/directors" element={<Directors />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
