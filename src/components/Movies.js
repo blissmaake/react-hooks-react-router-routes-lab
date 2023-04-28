f69d9c04d69144ba2fe07aebae75ba2b2281b80e
Bliss <blissmaake140@gmail.com>
2023-04-28 16:04:43 +0300

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,12 @@
-import React from "react";
-import { movies } from "../data";
+import React from "react"
+import { movies } from "../data"
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+    </div>
+  )
 }
 
-export default Movies;
+export default Movies
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,35 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from "react"
+import { NavLink } from "react-router-dom"
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact4="true" activestyle={{ background: "darkblue" }}>
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact="true"
+        activestyle={{ background: "darkblue" }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact="true"
+        activestyle={{ background: "darkblue" }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact="true"
+        activestyle={{ background: "darkblue" }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  )
 }
 
-export default NavBar;
+export default NavBar
